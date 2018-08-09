32e60fec900e98869fa2edc283cf107c068d01b4
liweipei <778323309@qq.com>
2018-08-09 19:13:04 +0800

    20180809191304

--- docker/apache/server/tools/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$server = new swoole_server("0.0.0.0", 91);
-$server->on('connect', function ($serv, $fd) {echo "Client: Connect.\n";});
-$server->on('receive', function ($serv, $fd, $from_id, $data) {$serv->send($fd, "Server: ".$data);});
-$server->on('close', function ($serv, $fd) {echo "Client: Close.\n";});
-$server->start();
