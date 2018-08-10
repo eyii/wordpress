31ad0bee15632a870dd385743cddb3b4e32b7afe
liweipei <778323309@qq.com>
2018-08-10 16:53:04 +0800

    20180810165304

--- gp.cmd
+++ gp.cmd
@@ -1,4 +1,4 @@
-git add . && git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%  " && git push bit master -f
+git config --global credential.helper store&&git add . && git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%  " && git push bit master -f
 
 
 
